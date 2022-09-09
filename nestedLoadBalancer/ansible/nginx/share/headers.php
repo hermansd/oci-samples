98f2be17c9f3cdec3c1aeba7cc33682b879c2cf4
Oracle Public Cloud User <opc@nginx1.web.hub.oraclevcn.com>
2022-09-09 21:53:58 +0000

    Adding the webserver config files

---
+++ nestedLoadBalancer/ansible/nginx/share/headers.php
@@ -0,0 +1,8 @@
+<pre>
+<?php
+
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+
+?></pre>
---
+++ nestedLoadBalancer/ansible/nginx/share/index.php
@@ -0,0 +1,58 @@
+
+<html>
+<body>
+    <H1>Campus1</H1>
+    <ul>
+        <li><a href="https://degreeworks.nosquabble.com/DashboardServlet/campus1_PROD/" target="_blank"> https://degreeworks.nosquabble.com/DashboardServlet/campus1_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/dwShell/campus1_PROD/" target="_blank">https://degreeworks.nosquabble.com/dwShell/campus1_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Controller_campus1/" target="_blank">https://degreeworks.nosquabble.com/Controller_campus1/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/ScribeUI/campus1_PROD/" target="_blank">https://degreeworks.nosquabble.com/ScribeUI/campus1_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/TransitUI_campus1/" target="_blank">https://degreeworks.nosquabble.com/TransitUI_campus1/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Composer_campus1/" target="_blank">https://degreeworks.nosquabble.com/Composer_campus1/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Dashboard_campus1/" target="_blank">https://degreeworks.nosquabble.com/Dashboard_campus1/</a></li>
+    </ul>
+    <H1>Campus2</H1>
+    <ul>
+        <li><a href="https://degreeworks.nosquabble.com/DashboardServlet/campus2_PROD/" target="_blank"> https://degreeworks.nosquabble.com/DashboardServlet/campus2_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/dwShell/campus2_PROD/" target="_blank">https://degreeworks.nosquabble.com/dwShell/campus2_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Controller_campus2/" target="_blank">https://degreeworks.nosquabble.com/Controller_campus2/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/ScribeUI/campus2_PROD/" target="_blank">https://degreeworks.nosquabble.com/ScribeUI/campus2_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/TransitUI_campus2/" target="_blank">https://degreeworks.nosquabble.com/TransitUI_campus2/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Composer_campus2/" target="_blank">https://degreeworks.nosquabble.com/Composer_campus2/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Dashboard_campus2/" target="_blank">https://degreeworks.nosquabble.com/Dashboard_campus2/</a></li>
+    </ul>
+    <H1>Campus3</H1>
+    <ul>
+        <li><a href="https://degreeworks.nosquabble.com/DashboardServlet/campus3_PROD/" target="_blank"> https://degreeworks.nosquabble.com/DashboardServlet/campus3_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/dwShell/campus3_PROD/" target="_blank">https://degreeworks.nosquabble.com/dwShell/campus3_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Controller_campus3/" target="_blank">https://degreeworks.nosquabble.com/Controller_campus3/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/ScribeUI/campus3_PROD/" target="_blank">https://degreeworks.nosquabble.com/ScribeUI/campus3_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/TransitUI_campus3/" target="_blank">https://degreeworks.nosquabble.com/TransitUI_campus3/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Composer_campus3/" target="_blank">https://degreeworks.nosquabble.com/Composer_campus3/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Dashboard_campus3/" target="_blank">https://degreeworks.nosquabble.com/Dashboard_campus3/</a></li>
+    </ul>
+    <H1>Campus4</H1>
+    <ul>
+        <li><a href="https://degreeworks.nosquabble.com/DashboardServlet/campus4_PROD/" target="_blank"> https://degreeworks.nosquabble.com/DashboardServlet/campus4_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/dwShell/campus4_PROD/" target="_blank">https://degreeworks.nosquabble.com/dwShell/campus4_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Controller_campus4/" target="_blank">https://degreeworks.nosquabble.com/Controller_campus4/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/ScribeUI/campus4_PROD/" target="_blank">https://degreeworks.nosquabble.com/ScribeUI/campus4_PROD/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/TransitUI_campus4/" target="_blank">https://degreeworks.nosquabble.com/TransitUI_campus4/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Composer_campus4/" target="_blank">https://degreeworks.nosquabble.com/Composer_campus4/</a></li>
+        <li><a href="https://degreeworks.nosquabble.com/Dashboard_campus4/" target="_blank">https://degreeworks.nosquabble.com/Dashboard_campus4/</a></li>
+    </ul>
+</body>
+
+
+<pre>
+<?php
+
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+
+?></pre>
+
+
+</html>
+
